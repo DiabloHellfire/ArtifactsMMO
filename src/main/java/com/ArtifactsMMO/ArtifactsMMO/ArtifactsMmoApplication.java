ca514730c09d8fb96fd1ef40c623febc18809557
Gheras <guillaume.heras@gmail.com>
2024-10-17 19:26:41 +0200

    Artifact Start SpringBoot Block

---
+++ src/main/java/com/ArtifactsMMO/ArtifactsMMO/ArtifactsMmoApplication.java
@@ -0,0 +1,13 @@
+package com.ArtifactsMMO.ArtifactsMMO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtifactsMmoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtifactsMmoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ArtifactsMMO/ArtifactsMMO/ArtifactsMmoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ArtifactsMMO.ArtifactsMMO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtifactsMmoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
